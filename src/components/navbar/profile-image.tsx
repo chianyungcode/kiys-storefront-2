c656642f7362e589c52e7f27e0beccabb453a39a
chianyungcode <cnytechcode@gmail.com>
2024-09-19 16:39:37 +0700

    feat: add dropdown menu for profile image with logout functionality in navbar

--- src/components/navbar/profile-image.tsx
+++ src/components/navbar/profile-image.tsx
@@ -1,12 +1,48 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const ProfileImage = () => {
+  const handleLogout = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/auth/logout`,
+        {
+          method: "POST",
+          credentials: "include", // Agar cookie di browser terbawa saat mengirim request logout, tanpa ini tidak akan berjalan
+        }
+      );
+
+      console.log(response.json());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="w-10 h-10 rounded-full border border-black overflow-hidden">
-      <img
-        src="https://api.dicebear.com/9.x/notionists-neutral/svg?seed=Jasmine"
-        alt="avatar"
-        className="object-cover h-full w-full"
-      />
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="w-10 h-10 rounded-full border border-black overflow-hidden">
+          <img
+            src="https://api.dicebear.com/9.x/notionists-neutral/svg?seed=Jasmine"
+            alt="avatar"
+            className="object-cover h-full w-full"
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent side="bottom" align="end" sideOffset={10}>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>My profile</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
