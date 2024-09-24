595b04eef736e6d0803ed33cb41445e8cd4790fd
chianyungcode <cnytechcode@gmail.com>
2024-09-24 14:33:11 +0700

    feat: update response types to include mandatory pagination in API responses with improved type definitions

--- src/types/response.ts
+++ src/types/response.ts
@@ -5,19 +5,27 @@ interface Pagination {
   limit: number;
 }
 
-interface SuccessResponseType<T> {
+interface ApiResponseWithPagination<T> {
+  success: boolean;
   message: string;
   data?: T;
-  pagination?: Pagination;
+  pagination: Pagination;
 }
+
+type ApiResponseData<T> = Pick<
+  ApiResponseWithPagination<T>,
+  "message" | "data" | "pagination"
+>;
 
 export const successResponse = <T>({
   data,
   message,
-}: SuccessResponseType<T>) => {
+  pagination,
+}: ApiResponseData<T>): ApiResponseWithPagination<T> => {
   return {
     success: true,
     message,
     data,
+    pagination,
   };
 };
